158068d6de53e8a5f377dc2af03204e3653b983b
ajith.woodenclouds <ajith.woodenclouds@gmail.com>
2026-03-09 23:01:46 +0530

    "feat: add circular loader component"

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
 import { ConfigProvider } from "./context/ConfigContext";
 import { ThemeProvider } from "./context/ThemeContext";
 import appConfig from "./config/AppConfig";
 import Navbar from "./components/NavBar";
-import PageRenderer from "./components/PageRenderer";
+import AGShopLoader from "./components/Loader/AGShopLoader";
+
+const PageRenderer = lazy(() => import("./components/PageRenderer"));
 
 const App = () => (
   <ConfigProvider>
     <ThemeProvider>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          {appConfig.pages.map((page) => (
-            <Route key={page.path} path={page.path} element={<PageRenderer />} />
-          ))}
-          <Route path="*" element={<PageRenderer />} />
-        </Routes>
+
+        <Suspense fallback={<div><AGShopLoader/></div>}>
+          <Routes>
+            {appConfig.pages.map((page) => (
+              <Route key={page.path} path={page.path} element={<PageRenderer />} />
+            ))}
+            <Route path="*" element={<PageRenderer />} />
+          </Routes>
+        </Suspense>
+
       </BrowserRouter>
     </ThemeProvider>
   </ConfigProvider>
